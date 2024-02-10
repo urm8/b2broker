51747279832178f755baada1c8e6bd413cb46b68
max <megafukz@gmail.com>
2024-02-10 17:48:56 +0300

    add fixtures

--- app/run.sh
+++ app/run.sh
@@ -3,4 +3,5 @@
 python manage.py collectstatic --noinput &
 python manage.py compilemessages &
 python manage.py migrate &
+python manage.py loaddata --format jsonl initial.jsonl &2 > /dev/null &
 exec "$@"
--- app/wallet/migrations/run_python/generate_wallets.py
+++
@@ -1,5 +0,0 @@
-
-
-def code(*args, **kwargs):
-    from wallet.factories import WalletFactory
-    WalletFactory.create_batch(100, transaction_set__size=500)
